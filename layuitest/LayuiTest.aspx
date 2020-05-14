c553fae824454108692cb9cdb9c06a9da9f462d9
xmgChauncey <1619112212@qq.com>
2020-05-14 16:06:46 +0800

    添加项目文件。

---
+++ layuitest/LayuiTest.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LayuiTest.aspx.cs" Inherits="layuitest.LayuiTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>layuitest</title>
+    <link rel="stylesheet" href="../layui/css/layui.css" />
+</head>
+<body>
+    <script src="./layui/layui.js"></script>
+</body>
+</html>
---
+++ layuitest/LayuiTest.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace layuitest
+{
+    public partial class LayuiTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        public static string GetValue()
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("name1", "001");
+            dic.Add("name2", "002");
+            string jsonStr = Newtonsoft.Json.JsonConvert.SerializeObject(dic);
+            return jsonStr;
+
+        }
+    }
+}
